47e2cd2678017f2bf7ae0fd47f27e4da76fb523c
Sam Sek <samsek123@gmail.com>
2019-03-08 02:52:56 +0800

    Build Website

---
+++ build/precache-manifest.fdf8d2904aeeb06962f08952ec4c45ad.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d156cb900ca6ffd49e34bcb8861e58a4",
+    "url": "/static/media/skill_title.d156cb90.png"
+  },
+  {
+    "revision": "562353143a656c6729f9",
+    "url": "/static/css/main.e65a8804.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ff57bcd9b896f37c9d21",
+    "url": "/static/js/2.ff57bcd9.chunk.js"
+  },
+  {
+    "revision": "25ef56c06eb39e136e1e60c528af7eb6",
+    "url": "/static/media/logo_white.25ef56c0.png"
+  },
+  {
+    "revision": "1fa3b785741f00676790cdedab452b8e",
+    "url": "/static/media/palapal.1fa3b785.png"
+  },
+  {
+    "revision": "d75cc11f37ed2e7ebf1812cdd5ee797e",
+    "url": "/static/media/savetify.d75cc11f.png"
+  },
+  {
+    "revision": "6fca04b395319868a0542e69a3f0628f",
+    "url": "/static/media/gamechat.6fca04b3.png"
+  },
+  {
+    "revision": "3a3b072658b3b68863c93713e69b5b56",
+    "url": "/static/media/earth.3a3b0726.svg"
+  },
+  {
+    "revision": "c14dc95dfbebbff6a80d5ad5db00cedb",
+    "url": "/static/media/coder.c14dc95d.png"
+  },
+  {
+    "revision": "fdbc14afbe3af2ac31ddb77daaba3498",
+    "url": "/static/media/profile.fdbc14af.jpg"
+  },
+  {
+    "revision": "562353143a656c6729f9",
+    "url": "/static/js/main.56235314.chunk.js"
+  },
+  {
+    "revision": "0f81171857bc477a91f1212f35e80833",
+    "url": "/static/media/skill_bg.0f811718.png"
+  },
+  {
+    "revision": "48bfab9c029db9fd2763ee98e1d6cec9",
+    "url": "/static/media/skill_bg_mobile.48bfab9c.png"
+  },
+  {
+    "revision": "65be977c6e433ed58c635ac99c60015f",
+    "url": "/static/media/fullstack.65be977c.png"
+  },
+  {
+    "revision": "a772f7715d4e38343f03639700291324",
+    "url": "/static/media/motiongraphic.a772f771.png"
+  },
+  {
+    "revision": "1ba6c22c9079538b467dc542c31bbfb8",
+    "url": "/static/media/uiux.1ba6c22c.png"
+  },
+  {
+    "revision": "ef50ae1c54cc56cc68dcaa9cc718f390",
+    "url": "/static/media/videoediting.ef50ae1c.png"
+  },
+  {
+    "revision": "ec373f4158c1408200f65954b5a4f76e",
+    "url": "/static/media/samsek.ec373f41.png"
+  },
+  {
+    "revision": "6e5706183c7c9f6414562b9f2406a6c9",
+    "url": "/static/media/utravel.6e570618.png"
+  },
+  {
+    "revision": "73407cb0337d9874b703781023a0fd0e",
+    "url": "/static/media/vottle.73407cb0.png"
+  },
+  {
+    "revision": "35e445185d11f26d86e8b7474579b10a",
+    "url": "/static/media/mircomodule.35e44518.png"
+  },
+  {
+    "revision": "afa718ddee3b5814471e7eb0ef2d4a20",
+    "url": "/index.html"
+  }
+];
